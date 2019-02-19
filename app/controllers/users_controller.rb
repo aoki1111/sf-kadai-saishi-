b1a6c46f8b2a12da72a52dc226d31c9c3518948c
makoto.kawamata <kawaman.program@gmail.com>
2019-02-19 10:49:24 +0900

    ユーザー登録できない、パスワードは８文字以上
    ユーザコントローラのストロングパラメータにnameがない、def createのメソッドのリダイレクト先がrootになｔっていた。root-urlに修正
    ユーザーのモデルにバリデーション（パスワードは8文字以上）を追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "ユーザー登録が完了しました"
-      redirect_to root
+      redirect_to root_url
     else
       flash.now[:danger] = "ユーザーの作成に失敗しました"
       render 'new'
@@ -52,7 +52,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password, :password_confirmation)
+    params.require(:user).permit(:name,:email, :password, :password_confirmation)
   end
 
   def login_user?
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,5 +6,5 @@ class User < ApplicationRecord
   validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX },length: { maximum: 100 }
-  
+  validates :password,presence: true,length: {minimum: 8}
 end
